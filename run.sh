9c11fc454b75ce7bdaee30b80444e9aa71f0bed9
Георгий Ищенко <ishchenko@ispras.ru>
2024-12-19 17:37:24 +0300

    changed compiler to clang++-18

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ mkdir -p build
 cmake -S . \
       -B build \
       -G Ninja \
-      -DCMAKE_CXX_COMPILER=clang++ \
-      -DCMAKE_C_COMPILER=clang \
+      -DCMAKE_CXX_COMPILER=clang++-18 \
+      -DCMAKE_C_COMPILER=clang-18 \
       -DTYPES="FLOAT,FAST_FIXED(13,7),FAST_FIXED(32,5),DOUBLE" \
       -DSIZES="S(36,84),S(10,10),S(42,1337)" \
       -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined -Wall -Wextra -pedantic -fprofile-instr-generate -fcoverage-mapping -O2" \
